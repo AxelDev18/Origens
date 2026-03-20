c1611e5e1a88b2f132d28d959d1bed7fc5ed59a1
Axel Freire Muñoz <afreire@alpesa.com>
2026-03-20 15:13:01 +0100

    feat: implementar router y navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Menu } from './components/menu/menu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'carta',
+        component: Menu
+    }
+];
